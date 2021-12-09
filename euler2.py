5f0501aab853b73786d8229608d5dae55790a409
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2021-12-09 09:15:52 -0800

    First commit -- base of code, will fix.

--- euler2.py
+++ euler2.py
@@ -1,1 +1,17 @@
-# This file exists now
+def fibonacci(base1=1, base2=2, loops=1):
+    if loops == 1:
+        return base1 + base2
+    else:
+        result = 0
+        last_sum1 = 0
+        last_sum2 = 0
+
+        for i in range(loops):
+            last_sum2 = last_sum1
+            last_sum1 = fibonacci(last_sum2, last_sum1)
+
+            result += last_sum1
+        return result
+
+if __name__ == '__main__':
+    print('Fibonacci calc is: ' + fibonacci(loops=4))
