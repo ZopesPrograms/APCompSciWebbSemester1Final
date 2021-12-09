ea090ef3b60480573ebbe9d93139b35a51d754ed
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2021-12-09 09:32:18 -0800

    Fibonacci calculation mostly working now.

--- euler2.py
+++ euler2.py
@@ -1,17 +1,21 @@
-def fibonacci(base1=1, base2=2, loops=1):
-    if loops == 1:
+def fibonacci(base1=1, base2=2, count=1):
+    if (count-2) == 1:
         return base1 + base2
     else:
         result = 0
-        last_sum1 = 0
-        last_sum2 = 0
+        last_sum1 = base2
+        last_sum2 = base1
 
-        for i in range(loops):
+        temp = 0
+
+        for i in range(count-2):
+            temp = fibonacci(last_sum2, last_sum1)
             last_sum2 = last_sum1
-            last_sum1 = fibonacci(last_sum2, last_sum1)
+            last_sum1 = temp
 
             result += last_sum1
-        return result
+            print((str(last_sum1) + ' +'), end =" ")
+        return result + (base1 + base2)
 
 if __name__ == '__main__':
-    print('Fibonacci calc is: ' + fibonacci(loops=4))
+    print('Fibonacci calc is: ' + str(fibonacci(count=5)))
