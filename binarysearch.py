787f66f87e891c46c269946ba3c287ff09ffaf02
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2021-12-13 22:12:35 -0800

    Code finished for binary search element found -- now need to keep track of index.

--- binarysearch.py
+++ binarysearch.py
@@ -1,5 +1,30 @@
-def binary_search(x, list):
-  assert type(list[0]) == float or type(list[0]) == int, (str(list) + ' must be numerical list!')
-  
-  partial_lists = []
-  divs_num = len(list)/2
+import math
+# lets fix this code
+
+def binarysearch(x, listv, recursive=False, index=None):
+    midval = 0
+    mid = math.ceil(len(listv)/2)-1
+
+    print(listv)
+
+    if mid == math.floor(len(listv)/2)-1:
+        midval = (listv[mid] + listv[mid+1])/2
+        if x < midval:
+            del listv[mid+1:]
+            return binarysearch(x, listv, True, mid)
+        elif x > midval:
+            del listv[:mid]
+            return binarysearch(x, listv, True, mid)
+    else:
+        midval = listv[mid]
+        if midval == x:
+            return mid
+        elif x < midval:
+            del listv[mid:]
+            return binarysearch(x, listv, True, mid)
+        elif x > midval:
+            del listv[:mid+1]
+            return binarysearch(x, listv, True, mid)
+
+if __name__ == '__main__':
+    print(binarysearch(7, [1,2,3,4,5,6,7,8,9,10]))
