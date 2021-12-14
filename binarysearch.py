9fe380a385e7d8117bab34902fcd53479e48f810
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2021-12-13 22:41:21 -0800

    Still can't fix the stupid offset.

--- binarysearch.py
+++ binarysearch.py
@@ -1,30 +1,36 @@
 import math
 # lets fix this code
 
-def binarysearch(x, listv, recursive=False, index=None):
+def binarysearch(x, listv, offset=0):
     midval = 0
     mid = math.ceil(len(listv)/2)-1
 
     print(listv)
+    print('Stupid offset is to ' + str(offset))
 
-    if mid == math.floor(len(listv)/2)-1:
+    if mid == math.floor(len(listv)/2) and mid != 0:
         midval = (listv[mid] + listv[mid+1])/2
+        print('Midval to: ' + str(midval))
         if x < midval:
+            print('Middle index is ' + str(mid + 1))
             del listv[mid+1:]
-            return binarysearch(x, listv, True, mid)
+            print('Listv to ' + str(listv))
+            return binarysearch(x, listv, mid+offset)
         elif x > midval:
+            print('Middle index is ' + str(mid))
             del listv[:mid]
-            return binarysearch(x, listv, True, mid)
+            print('Listv to ' + str(listv))
+            return binarysearch(x, listv, offset-mid)
     else:
         midval = listv[mid]
         if midval == x:
-            return mid
+            return mid+offset
         elif x < midval:
             del listv[mid:]
-            return binarysearch(x, listv, True, mid)
+            return binarysearch(x, listv, mid+offset)
         elif x > midval:
             del listv[:mid+1]
-            return binarysearch(x, listv, True, mid)
+            return binarysearch(x, listv, offset-mid)
 
 if __name__ == '__main__':
-    print(binarysearch(7, [1,2,3,4,5,6,7,8,9,10]))
+    print(binarysearch(7, [0,1,2,3,4,5,7,8,9,10]))
