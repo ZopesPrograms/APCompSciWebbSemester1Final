af0cd2d3b1122306a62e0fb389cb96ba1e382069
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2021-12-15 14:33:08 -0800

    IT WORKS NOW!! Yay.

--- binarysearch.py
+++ binarysearch.py
@@ -3,14 +3,15 @@ import math
 
 def binarysearch(x, listv, offset=0):
     midval = 0
-    mid = math.ceil(len(listv)/2)-1
+    mid = math.floor((len(listv)-1)/2)
 
     print(listv)
     print('Stupid offset is to ' + str(offset))
 
-    if mid == math.floor(len(listv)/2) and mid != 0:
-        midval = (listv[mid] + listv[mid+1])/2
+    if mid == len(listv)/2 and mid != 0:
+        midval = (listv[mid-1] + listv[mid])/2
         print('Midval to: ' + str(midval))
+
         if x < midval:
             print('Middle index is ' + str(mid + 1))
             del listv[mid+1:]
@@ -23,6 +24,8 @@ def binarysearch(x, listv, offset=0):
             return binarysearch(x, listv, offset+mid)
     else:
         midval = listv[mid]
+        print('Midval to: ' + str(midval))
+
         if midval == x:
             return mid+offset
         elif x < midval:
@@ -33,4 +36,4 @@ def binarysearch(x, listv, offset=0):
             return binarysearch(x, listv, offset+mid)
 
 if __name__ == '__main__':
-    print(binarysearch(7, [0,1,2,3,4,5,7,8,9,10]))
+    print(binarysearch(7, [0,1,2,7,8,9,10]))
