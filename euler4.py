84ca99a775b8b7452600ffd1d649dafb12e24c1d
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-01-04 21:00:57 -0800

    Changed names of files

--- euler4.py
+++ euler4.py
@@ -1,26 +1,19 @@
-from math import *
-
-def largest_prime_factor(num):
-    ''' Find largest prime factor of our integer num > 1.
-        
-    Input: an integer num > 1
-    Output: the largest prime factor in our num
-    '''
-
-    # Checks our number is a factorable integer > 1
-    assert type(num) == int, (str(num) + " is not an factorable integer!")
-    assert num > 1, (str(num) + " is not an factorable integer!")
-
-    ''' Finds smallest prime divisor of num, extracts it, then extracts the
-        next prime divisor of num through recursion then extracts the next
-        prime divisor, and so on until the last, largest prime divisor is
-        found. '''
-    for i in range(2, ceil(num/2)):
-        if num % i == 0:
-            return largest_prime_factor(int(num/i))
-    # Returns number if no divisor is found (num is prime, ergo it is largest prime)
-    return num
-
-# Main block, prints out our found largest prime factor
-if __name__ == '__main__':
-    print(largest_prime_factor(600851475143))
+''' THIS CODE WANTS TO BE THE VERY BEST,
+    LIKE NO CODE EVER WAS,
+    BA BA BA DA,
+    IT WANTS TO FIND THE BIGGEST PRIME
+    UNDER
+    600851475143,
+    PROBABLY DIVIDE THE NUMBER BY HALF,
+    USE PRIME SEARCHING ALGORITHMS,
+    TO FIND ALL PRIMES BELOW
+    600851475143,
+    AND SEE IF IT IS DIVISIBLE BY THEM,
+    MAYBE?
+    WHAT ELSE IS
+    600851475143
+    DIVISIBLE BY?
+    3 or 7 or etc.?
+    GOTTA FIGURE THAT OUT
+    FOR THE MATHS
+    TO FIND OUR QUARRY'S RANGE! '''
