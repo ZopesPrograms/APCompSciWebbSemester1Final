050fed860b2944f3609bb71bb5466c72f2fbf22f
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-01-04 20:41:56 -0800

    Finished algorithm code draft.

--- euler4.py
+++ euler4.py
@@ -1,9 +1,13 @@
-from typing import type_check_only
-
+from math import *
 
 def largest_prime_factor(num):
     assert type(num) == int, (str(num) + " is not an factorable integer!")
     assert num < 2, (str(num) + " is not an factorable integer!")
+
+    for i in range(0, ceil(num/2)):
+        if num % i == 0:
+            return num/i
+    return -1
 
 if __name__ == '__main__':
     print(largest_prime_factor(600851475143))
