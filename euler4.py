442df4a34c2a55610ad157d0ed12493f4531aa34
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-01-04 20:48:03 -0800

    Made code work w/recursion.

--- euler4.py
+++ euler4.py
@@ -4,10 +4,10 @@ def largest_prime_factor(num):
     assert type(num) == int, (str(num) + " is not an factorable integer!")
     assert num > 1, (str(num) + " is not an factorable integer!")
 
-    for i in range(0, ceil(num/2)):
+    for i in range(2, ceil(num/2)):
         if num % i == 0:
-            return num/i
-    return -1
+            return largest_prime_factor(int(num/i))
+    return num
 
 if __name__ == '__main__':
     print(largest_prime_factor(600851475143))
