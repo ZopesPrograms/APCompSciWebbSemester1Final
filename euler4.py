c05979d0677a4096f8f0d35ac13ed31cf659f03f
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-01-04 20:42:38 -0800

    Fixed calculation goof.

--- euler4.py
+++ euler4.py
@@ -2,7 +2,7 @@ from math import *
 
 def largest_prime_factor(num):
     assert type(num) == int, (str(num) + " is not an factorable integer!")
-    assert num < 2, (str(num) + " is not an factorable integer!")
+    assert num > 1, (str(num) + " is not an factorable integer!")
 
     for i in range(0, ceil(num/2)):
         if num % i == 0:
