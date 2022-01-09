27876b0590578b4fa9d6868d2855ce7adf11d94f
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-01-09 09:05:34 -0800

    Palindrome tester working fine

--- euler4.py
+++ euler4.py
@@ -7,18 +7,18 @@ def is_palindrome(num):
     middle = (len(digits) - 1) / 2
 
     if len(digits) % 2 == 0:
-        seg1 = ''.join(digits[:middle])
-        print('People who don\'t know be like: ' + seg1)
-
-        seg2 = ''.join(reversed(digits[middle+1:]))
-        print('People who know: ' + seg2)
-
-        return (seg1 == seg2)
-    else:
         seg1 = ''.join(digits[:ceil(middle)])
         print('People who don\'t know be like: ' + seg1)
 
         seg2 = ''.join(reversed(digits[ceil(middle):]))
+        print('People who know: ' + seg2)
+
+        return (seg1 == seg2)
+    else:
+        seg1 = ''.join(digits[:int(middle)])
+        print('People who don\'t know be like: ' + seg1)
+
+        seg2 = ''.join(reversed(digits[int(middle+1):]))
         print('People who know: ' + seg2)
 
         return (seg1 == seg2)
@@ -31,4 +31,4 @@ def find_largest_palindrome(base10):
 
 if __name__ == '__main__':
     #print(find_largest_palindrome(3))
-    print(is_palindrome(10101))
+    print(is_palindrome(12121))
