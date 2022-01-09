b145aea231c59839202ef68aced40fa198ab5e97
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-01-09 08:54:09 -0800

    Added palindrome test function -- work proceeds

--- euler4.py
+++ euler4.py
@@ -1,6 +1,22 @@
-def find_largest_palindrome():
-    digits = [9,9,9,9,9,9]
+from math import *
+
+def is_palindrome(num):
+    assert len(num) > 1, 'Palindrome must have more than one element to compare'
+
+    digits = list(str(num))
+    middle = (len(num) - 1) / 2
+
+    if len(num) % 2 == 0:
+        seg1 = ''.join(digits[:ceil(middle)])
+        seg2 = ''.join(digits[ceil(middle):])
+
+        return (seg1 == seg2)
+
+
+def find_largest_palindrome(base10):
+    toolarge = 10**(2 * base10)
+
     return -1
 
 if __name__ == '__main__':
-    print(find_largest_palindrome())
+    print(find_largest_palindrome(3))
