13e8c47244efd8145c529f713d7ec0d162de0fdc
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-01-11 22:30:06 -0800

    Added some palindrome-splitting and factoring functionality.

--- euler4.py
+++ euler4.py
@@ -1,6 +1,21 @@
 from math import *
 
-def is_palindrome(num):
+def factor_int(num):
+    
+    assert type(num) == int, (str(num) + " is not an factorable integer!")
+    assert num > 1, (str(num) + " is not an factorable integer!")
+
+    ''' Finds smallest prime divisor of num, extracts it, then extracts the
+        next prime divisor of num through recursion then extracts the next
+        prime divisor, and so on until the last, largest prime divisor is
+        found. '''
+    for i in range(2, ceil(num/2)):
+        if num % i == 0:
+            return [i] + factor_int(int(num/i))
+    # Returns number if no divisor is found (num is prime, ergo it is largest prime)
+    return [num]
+
+def is_palindrome(num, mustsplit=False):
     digits = list(str(num))
     assert len(digits) > 1, 'Palindrome must have more than one element to compare'
 
@@ -13,28 +28,31 @@ def is_palindrome(num):
         seg2 = ''.join(reversed(digits[ceil(middle):]))
         print('People who know: ' + seg2)
 
+        if mustsplit:
+            return seg1
+
         return (seg1 == seg2)
     else:
         seg1 = ''.join(digits[:int(middle)])
         print('People who don\'t know be like: ' + seg1)
+
+        if mustsplit:
+            return seg1
 
         seg2 = ''.join(reversed(digits[int(middle+1):]))
         print('People who know: ' + seg2)
 
         return (seg1 == seg2)
 
-
 def find_largest_palindrome(base10):
     toolarge = 10**(2 * base10)
-    toosmall = 10**(2(base10-1))
+    toosmall = 10**(2*(base10-1))
 
     last_palindrome = toolarge-1
-    digits = list(str(last_palindrome))
-
-    while last_palindrome > toosmall:
+    print(is_palindrome(last_palindrome,True))
 
     return -1
 
 if __name__ == '__main__':
-    #print(find_largest_palindrome(3))
+    print(find_largest_palindrome(3))
     print(is_palindrome(12121))
